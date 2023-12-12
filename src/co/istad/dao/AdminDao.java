3599440b6594a1b553e7ef60ed8ca616f1958dfd
vanndeth <vanndeth@gmail.com>
2023-12-12 13:12:55 +0700

    remove and disable feature

--- src/co/istad/dao/AdminDao.java
+++ src/co/istad/dao/AdminDao.java
@@ -17,6 +17,6 @@ public interface AdminDao extends MainFeatureDao {
     public void getReport();
     public void resetPassword(User user);
     public void disableAccount(User user);
-    public User removeAccount(User user);
+    public void removeAccount(User user);
     public void saveReportAsExcel();
 }
